a716e482c852d8b90ed9fbc22fc076a632986973
AshuCtrlZ <ashu.work012@gmail.com>
2026-07-08 00:52:51 +0530

    Add files via upload
    Updated the conditionals

--- bmi.py
+++ bmi.py
@@ -1,11 +1,11 @@
 
 #making a bmi calculator
-weight = float(input("What is your weight (kg)? "))
-height = float(input("What is your height (m)? "))
+weight = float(input("What is your weight (kg)? ")).strip()
+height = float(input("What is your height (m)? ")).strip()
 
 bmi = weight/(height ** 2)
 
-print(f" BMI = {round(bmi, 2)}")
+print(f"BMI = {round(bmi, 2)}")
 
 if bmi < 18.5:
     print("Underweight")
